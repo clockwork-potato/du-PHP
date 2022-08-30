585031fff339a4ee39bc3a9ef0128f2e813989a7
Emestay <103575914+Emestay@users.noreply.github.com>
2022-08-30 11:24:25 +0200

    test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ switch ($i) {
         echo "i égal 0";
         break;
     case 1:
-        echo "i égal 1";
+        echo "i égal 1 ! ";
         break;
     case 2:
         echo "<h1>i égal 2</h1>";
