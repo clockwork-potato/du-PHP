bbee49d7508cbaf0961cbb3c335575b073d27273
Emestay <103575914+Emestay@users.noreply.github.com>
2022-09-02 12:33:46 +0200

    composant php nav header footer

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+//! L'ORDRE EST IMPORTANT
+
+include './utils/dbConnect.php';
+
+include './model/utilisateur.php';
+include './view/view_header.php';
+include './view/view_navbar.php';
+include './view/view_home.php';
+include './view/view_footer.php';
+
+
+
+
+?>
