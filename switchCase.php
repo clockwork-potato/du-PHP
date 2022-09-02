9082ba91be38d2b71d420bb038a1958df1ffeb81
Emestay <103575914+Emestay@users.noreply.github.com>
2022-09-02 12:15:49 +0200

    index & switch case

---
+++ switchCase.php
@@ -0,0 +1,23 @@
+<?php
+// Ce switch:
+   // echo'<p><input type="submit" value="$i"></p>'; marche pas 
+   $i = 2;
+    
+switch ($i) {
+    case 0:
+        echo "i égal 0";
+        break;
+    case 1:
+        echo "i égal 1 ! ";
+        break;
+    case 2:
+        echo "<h1>i égal 2</h1>";
+        break;
+}
+
+?>
+
+<?php
+    echo "coucou Lucas";
+    echo "coucou Lucas";
+?>
