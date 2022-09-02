d79e5218a105e8b532449205b86841e618e2f5dd
Emestay <103575914+Emestay@users.noreply.github.com>
2022-09-02 09:57:58 +0200

    changement

--- tvaExo.php
+++ tvaExo.php
@@ -27,12 +27,7 @@ $tva = $_POST['TVA'];
 
 
 
-/*-Créer une page de formulaire dans laquelle on aura 3 champs de formulaire de type nombre :
-1 champ de formulaire qui demande un prix HT d’un article,
-1 champ de formulaire qui demande le nombre d’article,
-1 champ de formulaire qui demande le taux de TVA,
--Afficher dans cette même page le prix TTC (prix HT*taux TVA*quantité) avec un affichage du style :
-Le prix TTC est égal à : valeur €.*/
+
 ?>
 <style>
     form{
